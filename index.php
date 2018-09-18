5ff1786d9b6c3bca08078e25d77c32062ef2306d
KevinAssink <kevin.assink@student.rocvf.nl>
2018-09-18 14:05:46 +0200

    finished base site

--- index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-</head>
